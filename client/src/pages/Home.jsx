55c841def68184830f8a7aad9340f585d6ef25de
Udanth <udanth.work@gmail.com>
2025-10-29 18:40:40 +0530

    add home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
+import Testimonial from '../components/home/Testimonial'
 
 const Home = () => {
   return (
     <div>
-      Home Page
+      <Banner/>
+      <Hero/>
+      <Features/>
+      <Testimonial/>
     </div>
   )
 }
