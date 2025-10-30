804ba0eaa1a5218cf54820c1c2fa0fa1dff6b654
Udanth <udanth.work@gmail.com>
2025-10-30 07:59:02 +0530

    add footer

--- client/src/components/home/Testimonial.jsx
+++ client/src/components/home/Testimonial.jsx
@@ -108,7 +108,7 @@ const Testimonial = () => {
                         <CreateCard key={`row2-${index}`} card={card} />
                     ))}
                 </div>
-                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent"></div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-linear-to-l from-white to-transparent"></div>
             </div>
         </div>
     )
